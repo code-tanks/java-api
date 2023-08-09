3fe49f897c7102e87de6a0741b7cb090cf9dd55c
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-08-08 23:14:51 -0700

    Update Command.java

--- src/main/java/api/Command.java
+++ src/main/java/api/Command.java
@@ -1,20 +1,20 @@
 package api;
 
 public class Command {
-    static final int NONE = 0;
-    static final int MOVE_FORWARD = 1 << 0;
-    static final int MOVE_BACKWARD = 1 << 1;
-    static final int ROTATE_TANK_CLOCKWISE = 1 << 2;
-    static final int ROTATE_TANK_COUNTER_CLOCKWISE = 1 << 3;
-    static final int FIRE = 1 << 4;
-    static final int ROTATE_GUN_CLOCKWISE = 1 << 5;
-    static final int ROTATE_GUN_COUNTER_CLOCKWISE = 1 << 6;
-    static final int ROTATE_RADAR_CLOCKWISE = 1 << 7;
-    static final int ROTATE_RADAR_COUNTER_CLOCKWISE = 1 << 8;
-    static final int LOCK_GUN = 1 << 9;
-    static final int UNLOCK_GUN = 1 << 10;
-    static final int LOCK_RADAR = 1 << 11;
-    static final int UNLOCK_RADAR = 1 << 12;
-    static final int REQUEST_INFO = 1 << 13;
-    static final int CLEAR_COMMANDS = 1 << 14;
+    public static final int NONE = 0;
+    public static final int MOVE_FORWARD = 1 << 0;
+    public static final int MOVE_BACKWARD = 1 << 1;
+    public static final int ROTATE_TANK_CLOCKWISE = 1 << 2;
+    public static final int ROTATE_TANK_COUNTER_CLOCKWISE = 1 << 3;
+    public static final int FIRE = 1 << 4;
+    public static final int ROTATE_GUN_CLOCKWISE = 1 << 5;
+    public static final int ROTATE_GUN_COUNTER_CLOCKWISE = 1 << 6;
+    public static final int ROTATE_RADAR_CLOCKWISE = 1 << 7;
+    public static final int ROTATE_RADAR_COUNTER_CLOCKWISE = 1 << 8;
+    public static final int LOCK_GUN = 1 << 9;
+    public static final int UNLOCK_GUN = 1 << 10;
+    public static final int LOCK_RADAR = 1 << 11;
+    public static final int UNLOCK_RADAR = 1 << 12;
+    public static final int REQUEST_INFO = 1 << 13;
+    public static final int CLEAR_COMMANDS = 1 << 14;
 }
