6b1067a8045e03c6afcba122f95843dd4b927e2e
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-08-08 23:19:29 -0700

    Update MyTank.java

--- src/main/java/tanks/MyTank.java
+++ src/main/java/tanks/MyTank.java
@@ -4,32 +4,23 @@ import java.util.HashMap;
 
 import api.BaseTank;
 
-public class MyTank {
-    public static BaseTank createTank() {
-        return new Tank();
+public class Tank extends BaseTank {
+    public Tank() {
+        System.out.println("Started MyTank..");
     }
 
-    static class Tank extends BaseTank {
-        public Tank() {
-            System.out.println("Started MyTank..");
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'run'");
+        System.out.println("Run");
+    }
 
-        }
-
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
-            // throw new UnsupportedOperationException("Unimplemented method 'run'");
-            System.out.println("Run");
-
-        }
-
-        @Override
-        public void onEvent(HashMap<String, Object> event) {
-            // TODO Auto-generated method stub
-            // throw new UnsupportedOperationException("Unimplemented method 'onEvent'");
-            System.out.println("onEvent");
-            System.out.println(event);
-
-        }
+    @Override
+    public void onEvent(HashMap<String, Object> event) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'onEvent'");
+        System.out.println("onEvent");
+        System.out.println(event);
     }
 }
